1f0f836f6e18366909aa7c353c27b50171505913
GauBen <benaim.gautier@gmail.com>
2022-05-10 00:00:47 +0200

    fix: prevent fouc

--- src/routes/articles/[article].svelte
+++ src/routes/articles/[article].svelte
@@ -1,4 +1,5 @@
 <script lang="ts" context="module">
+  import { browser } from '$app/env'
   import { page } from '$app/stores'
   import Prism from '$lib/Prism.svelte'
   import type { Load } from '@sveltejs/kit'
@@ -27,19 +28,26 @@
 
     if (!module) return { status: 404 }
 
-    return {
-      stuff: module.metadata,
-      props: {
-        component: module.default,
-      },
-    }
+    const { metadata: stuff, default: component } = module
+    if (browser) return { stuff, props: { component } }
+
+    // Prerender and extract CSS to prevent FOUC
+    const { css, head } = component.render()
+    return { stuff, props: { head, css: css.code, component } }
   }
 </script>
 
 <script lang="ts">
   export let component: SvelteComponent
+  export let head: string = ''
+  export let css: string = ''
   $: ({ title, snippet } = $page.stuff)
 </script>
+
+<svelte:head>
+  {@html head}
+  {@html '<' + 'style>' + css + '<' + '/style>'}
+</svelte:head>
 
 <h1>{title}</h1>
 {#if snippet}
