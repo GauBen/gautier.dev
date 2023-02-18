03ef6bc773bc71754d3183a9f75cffdd933f5d9e
Gautier <gautier@escape.tech>
2023-02-18 10:53:28 -0800

    fix: brought back type file

---
+++ src/app.d.ts
@@ -0,0 +1,12 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+  namespace App {
+    interface PageData {
+      title: string
+      description?: string
+    }
+  }
+}
+
+export {}
