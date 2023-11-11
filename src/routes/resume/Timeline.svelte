8b8887b5bda1c64324c96a5e1623e312cb83c451
Gautier <benaim.gautier@gmail.com>
2023-11-11 11:39:26 +0100

    fix: global selector preventing build

--- src/routes/resume/Timeline.svelte
+++ src/routes/resume/Timeline.svelte
@@ -8,17 +8,17 @@
     gap: 0.5em;
     align-items: center;
     margin-block: 0.25em;
+  }
 
-    &::before {
-      width: 0.25rem;
-      height: 0.25rem;
-      margin: 0.5rem;
-      content: "";
-      background: #0d3c77;
-      border-radius: 50%;
-      outline: 2px solid #0d3c77;
-      outline-offset: 0.375rem;
-    }
+  div :global(p::before) {
+    width: 0.25rem;
+    height: 0.25rem;
+    margin: 0.5rem;
+    content: "";
+    background: #0d3c77;
+    border-radius: 50%;
+    outline: 2px solid #0d3c77;
+    outline-offset: 0.375rem;
   }
 
   div :global(ul) {
