9c551b9256454fefd46a4e9cd39c4b96f5ecb87a
Gautier Ben Aim <benaim.gautier@gmail.com>
2024-10-03 18:22:02 +0200

    feat: prerender articles

--- src/routes/(blog)/articles/[slug]/+page.ts
+++ src/routes/(blog)/articles/[slug]/+page.ts
@@ -1,6 +1,10 @@
 import { articles } from "$lib/articles";
 import { error } from "@sveltejs/kit";
 
+export const prerender = true;
+
+export const entries = () => [...articles.keys()].map((slug) => ({ slug }));
+
 export const load = async ({ params }) => {
   const article = articles.get(params.slug);
   if (!article) error(404, "Article not found");
