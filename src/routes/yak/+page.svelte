46d4f09c190392993d8405390429e35a5bf29455
Gautier Ben Aim <benaim.gautier@gmail.com>
2024-12-20 13:36:26 +0100

    yak shaving map

---
+++ src/routes/yak/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Mermaid from "$lib/Mermaid.svelte";
+</script>
+
+<main style="text-align: center">
+  <h1>Yak Shaving Map</h1>
+
+  <!-- prettier-ignore -->
+  <Mermaid>
+    graph TD
+      timeline((Timeline)) --> formgator((Formgator))
+      timeline --> uistiti((Uistiti))
+  </Mermaid>
+</main>
