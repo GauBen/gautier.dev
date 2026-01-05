641852326c829b4e135ce3a135e86a98f0019c29
Gautier Ben Aïm <benaim.gautier@gmail.com>
2026-01-05 18:32:59 +0100

    feat: work on macos

--- adapter/src/index.js
+++ adapter/src/index.js
@@ -110,12 +110,19 @@ export default function adapter() {
 
       console.info(`node: ${prettyBytes(statSync(`${cwd}/node`).size)}`);
 
+      if (process.platform === "darwin") execFileSync("codesign", ["--remove-signature", `${cwd}/node`]);
+
       await inject(
         `${cwd}/node`,
         "NODE_SEA_BLOB",
         readFileSync(`${cwd}/bundle.blob`),
-        { sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2" },
+        {
+          sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2",
+          machoSegmentName: "NODE_SEA",
+        },
       );
+
+      if (process.platform === "darwin") execFileSync("codesign", ["--sign", "-", `${cwd}/node`]);
 
       console.info(`app: ${prettyBytes(statSync(`${cwd}/node`).size)}`);
 
