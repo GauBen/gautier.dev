d953e9f56cc71bbe36b13bad74ae5198fcb73a9f
Gautier Ben Aim <benaim.gautier@gmail.com>
2026-01-09 19:24:52 +0100

    bump

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-// import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "adapter-node-sea";
 import { mdsvex } from "mdsvex";
