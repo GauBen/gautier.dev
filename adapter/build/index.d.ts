cd049917720dbf59041cfc5323d2e0b60c437043
Gautier Ben Aim <benaim.gautier@gmail.com>
2026-03-25 23:35:19 +0100

    build: fix adapter tsconfig

--- adapter/build/index.d.ts
+++
@@ -1,5 +0,0 @@
-/** @returns {import("@sveltejs/kit").Adapter} */
-export default function adapter({ precompress, envPrefix }?: {
-    precompress?: boolean | undefined;
-    envPrefix?: string | undefined;
-}): import("@sveltejs/kit").Adapter;
