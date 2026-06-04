b92bd62c48d1ea27d93921b19449017556ec8ef7
Gautier Ben Aim <benaim.gautier@gmail.com>
2026-06-04 23:48:06 +0200

    perf: link preload for fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,24 @@
   import { page } from "$app/state";
   import "../app.css";
 
+  import technaUrl from "$assets/techna-sans.woff2?url";
+  import oxprotoRegularUrl from "$assets/0xproto-regular.woff2?url";
+  import oxprotoItalicUrl from "$assets/0xproto-italic.woff2?url";
+
   const { children } = $props();
   const { title, description } = $derived(page.data);
 </script>
 
 <svelte:head>
+  {#each [technaUrl, oxprotoRegularUrl, oxprotoItalicUrl] as font}
+    <link
+      rel="preload"
+      href={font}
+      as="font"
+      type="font/woff2"
+      crossorigin="anonymous"
+    />
+  {/each}
   {#if title}
     <title>{title} – gautier.dev</title>
     <meta property="og:title" content={title} />
